9126d9376e3d9bcae11d38abdfc853f91a66dcb7
Saddock Kabandana <aimegetz@gmail.com>
2026-02-11 10:34:01 +0200

    update dockerfile

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -7,7 +7,6 @@ const connectDB = async () => {
   try {
     const uri = process.env.MONGO_URI || 'mongodb://localhost:27017/codemande_hub';
     const conn = await mongoose.connect(uri);
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${(error as Error).message}`);
     process.exit(1);
